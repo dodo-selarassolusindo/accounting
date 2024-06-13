6a4ddfcc52c98077e3bdb315e59d0224166e41e1
Your Name <you@example.com>
2024-06-13 14:56:14 +0700

    .

--- models/Jurnal.php
+++ models/Jurnal.php
@@ -1695,6 +1695,20 @@ class Jurnal extends DbTable
     public function rowRendering()
     {
         // Enter your code here
+        if ($this->CurrentAction == 'add') {
+            // isi nomor jurnal
+            $tahunBulan = date('ym');
+            $lastNomor = ExecuteScalar("SELECT MAX(nomer) AS lastNomor FROM jurnal WHERE nomer LIKE '%" . $tahunBulan . "%'");
+            // jika ada nomer untuk tahun-bulan saat ini, ambil angka terakhir dan tambah 1
+            if ($lastNomor) {
+                $num = (int)substr($lastNomor, 6) + 1; // ambil bagian angka setelah tahun-bulan
+            } else {
+                $num = 1; // jika tidak ada Nomor untuk tahun-bulan saat ini, mulai dari 1
+            }
+            // format Nomor baru
+            $newNomor = 'JU' . $tahunBulan . str_pad($num, 3, '0', STR_PAD_LEFT);
+            $this->nomer->EditValue = $newNomor;
+        }
     }
 
     // Row Rendered event
--- models/JurnalAdd.php
+++ models/JurnalAdd.php
@@ -1214,19 +1214,6 @@ class JurnalAdd extends Jurnal
         //Log("Page Load");
         $this->period_id->DisplayValueSeparator = ' - ';
         $this->nomer->ReadOnly = true;
-
-        // isi nomor jurnal
-        $tahunBulan = date('ym');
-        $lastNomor = ExecuteScalar("SELECT MAX(nomer) AS lastNomor FROM jurnal WHERE nomer LIKE '%" . $tahunBulan . "%'");
-        // jika ada nomer untuk tahun-bulan saat ini, ambil angka terakhir dan tambah 1
-        if ($lastNomor) {
-            $num = (int)substr($lastNomor, 6) + 1; // ambil bagian angka setelah tahun-bulan
-        } else {
-            $num = 1; // jika tidak ada Nomor untuk tahun-bulan saat ini, mulai dari 1
-        }
-        // format Nomor baru
-        $newNomor = 'JU' . $tahunBulan . str_pad($num, 3, '0', STR_PAD_LEFT);
-        $this->nomer->EditValue = $newNomor;
     }
 
     // Page Unload event
