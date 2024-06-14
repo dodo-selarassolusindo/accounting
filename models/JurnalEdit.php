9d3c495ac22ff58d34b40901c676b6794a8d61ab
Your Name <you@example.com>
2024-06-14 16:48:36 +0700

    .

--- models/JurnalEdit.php
+++ models/JurnalEdit.php
@@ -1537,6 +1537,9 @@ class JurnalEdit extends Jurnal
         //Log("Page Load");
         $this->period_id->DisplayValueSeparator = ' - ';
         $this->nomer->ReadOnly = true;
+        $this->debet->ReadOnly = true;
+        $this->kredit->ReadOnly = true;
+        $this->selisih->ReadOnly = true;
     }
 
     // Page Unload event
